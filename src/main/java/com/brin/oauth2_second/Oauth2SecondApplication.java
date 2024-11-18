20daa9b37fc0a444ca4217daf89871903e153d0d
Brin29 <breinerstivenparracortes50@gmail.com>
2024-11-18 09:33:16 -0500

    first commit

---
+++ src/main/java/com/brin/oauth2_second/Oauth2SecondApplication.java
@@ -0,0 +1,13 @@
+package com.brin.oauth2_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SecondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brin/oauth2_second/Oauth2SecondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brin.oauth2_second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
