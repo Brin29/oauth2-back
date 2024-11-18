a04e129347504d60d2f142f701feb54389c6ef9a
Brin29 <breinerstivenparracortes50@gmail.com>
2024-11-18 10:24:25 -0500

    sas

---
+++ src/main/java/com/brin/oauth2_second/controllers/PublicController.java
@@ -0,0 +1,15 @@
+package com.brin.oauth2_second.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublicController {
+
+    @GetMapping("/public/messages")
+    public String publicMessages(Model model){
+        model.addAttribute("body", "nobody");
+        return "response";
+    }
+}
